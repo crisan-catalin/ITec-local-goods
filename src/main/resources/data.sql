43851520562ff50109c892cd7b535d0aeb60b389
Catalin Crisan <catalin.crisan@iquestgroup.com>
2019-11-22 20:17:05 +0200

    add categories sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO `categories` (`id`, `name`, `supercategory_id`) VALUES
+(1, 'Legume', NULL),
+(2, 'Morcovi', 1),
+(3, 'Rosii', 1),
+(4, 'Castraveti', 1),
+(5, 'Cartofi', 1),
+(6, 'Fructe', NULL),
+(7, 'Mere', 6),
+(8, 'Struguri', 6),
+(9, 'Pere', 6);
+
