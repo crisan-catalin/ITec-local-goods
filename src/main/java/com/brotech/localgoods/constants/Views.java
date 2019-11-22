af9474d9ae79d003637f1748d6b5154bb67fb8c9
Catalin Crisan <catalin.crisan@iquestgroup.com>
2019-11-23 00:20:13 +0200

    Add constant for ADD PRODUCT PAGE view

--- src/main/java/com/brotech/localgoods/constants/Views.java
+++ src/main/java/com/brotech/localgoods/constants/Views.java
@@ -5,5 +5,5 @@ public class Views {
     public static final String MAIN_PAGE = "index";
     public static final String LOGIN_PAGE = "login";
     public static final String REGISTER_PAGE = "register";
-
+    public static final String ADD_PRODUCT_PAGE = "add_product";
 }
