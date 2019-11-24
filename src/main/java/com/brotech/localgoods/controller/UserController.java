ce132e98c169a670155f344a9df7f451d64d30c8
nicolae.lungu <nicolae.lungu@iquestgroup.com>
2019-11-24 04:38:40 +0200

    -backend for user details

--- src/main/java/com/brotech/localgoods/controller/UserController.java
+++ src/main/java/com/brotech/localgoods/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.brotech.localgoods.controller;
 
 import com.brotech.localgoods.constants.Views;
+import com.brotech.localgoods.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    private static final String USER_DETAILS = "userDetails";
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{userId}")
-    public String getUserProfile(@PathVariable Long userId){
+    public String getUserProfile(@PathVariable Long userId, Model model) {
+        model.addAttribute(USER_DETAILS, userService.getUserById(userId));
         return Views.USER_PROFILE;
     }
 }
